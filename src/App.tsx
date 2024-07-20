591d8493fdf5a820c2721a8a04631a8d0a671d58
Cương <154745357+daokecuong1608@users.noreply.github.com>
2024-07-20 22:46:20 +0700

    navbar , footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './layouts/header-footer/Navbar';
+import Footer from './layouts/Footer';
 
 function App() {
   return (
     <div className="App">
-      <div className='bg-primary'>
-        Book store
-      </div>
+
+      {/* thanh điiều hướng (menu) navbar */}
+      <Navbar />
+
+      {/* //chân trang (footer) */}
+      <Footer />
+
     </div>
+
   );
 }
 
